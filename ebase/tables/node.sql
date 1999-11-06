0cc9f449542377d24cd81c21deeb96896f13e40c
dbrown <dbrown@b9a9f069-900d-0410-9e71-c61feadb8a10>
1999-11-06 10:41:20 +0000

    Checked in fix for ebase install
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@9 b9a9f069-900d-0410-9e71-c61feadb8a10

---
+++ ebase/tables/node.sql
@@ -0,0 +1,26 @@
+# MySQL dump 6.4
+#
+# Host: localhost    Database: everything
+#--------------------------------------------------------
+# Server version	3.22.27
+
+#
+# Table structure for table 'node'
+#
+CREATE TABLE node (
+  node_id int(11) DEFAULT '0' NOT NULL auto_increment,
+  type_nodetype int(11) DEFAULT '0' NOT NULL,
+  title char(240) DEFAULT '' NOT NULL,
+  author_user int(11) DEFAULT '0' NOT NULL,
+  createtime datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  hits int(11) DEFAULT '0',
+  reputation int(11) DEFAULT '0' NOT NULL,
+  lockedby_user int(11) DEFAULT '0' NOT NULL,
+  locktime datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  core char(1) DEFAULT '0',
+  package int(11) DEFAULT '0' NOT NULL,
+  PRIMARY KEY (node_id),
+  KEY title (title,type_nodetype),
+  KEY author (author_user),
+  KEY type (type_nodetype)
+);
---
+++ ebase/tables/setting.sql
@@ -0,0 +1,15 @@
+# MySQL dump 6.4
+#
+# Host: localhost    Database: everything
+#--------------------------------------------------------
+# Server version    3.22.27
+
+#
+# Table structure for table 'setting'
+#
+CREATE TABLE setting (
+  setting_id int(11) DEFAULT '0' NOT NULL auto_increment,
+  vars text NOT NULL,
+  PRIMARY KEY (setting_id)
+);
+
