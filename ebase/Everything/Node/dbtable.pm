c937edecc2fc6f0705b6d88166dec81b1dd28b25
nate <nate@b9a9f069-900d-0410-9e71-c61feadb8a10>
2000-10-04 20:42:57 +0000

    Added table length and allowed character checking code to insert.  Should stop dbtable nodes from being created though the underlying tables could not be.
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@129 b9a9f069-900d-0410-9e71-c61feadb8a10

--- ebase/Everything/Node/dbtable.pm
+++ ebase/Everything/Node/dbtable.pm
@@ -21,9 +21,33 @@ use strict;
 #		We need to create the table in the database.  This gets the
 #		node inserted into the database first, then creates the table.
 #
+#		Allowed characters and length limits are taken from MySQL table naming
+#		conventions.
+#
 sub insert
 {
 	my ($this, $USER) = @_;
+	my $title = $$this{title};
+
+	# sanity checks -- valid node titles may be invalid database table names
+	my $bad_title = 0;
+
+	# limit is 61 characters, as we append '_id' to title for primary key name
+	if (length $title > 61) {
+		Everything::logErrors('dbtable name must not exceed 61 characters!',
+			'', '', '');
+		$bad_title = 1;
+	}
+
+	# check for allowed characters
+	if ($title =~ tr/A-Za-z0-9_$//c ) {
+		Everything::logErrors('dbtable name contains invalid characters.
+		 	 Only alphanumerics and the underscore are allowed.  No spaces!',
+			 '', '', '');
+		$bad_title = 1;
+	}
+	return if $bad_title;
+
 	my $result = $this->SUPER();
 
 	$$this{DB}->createNodeTable($$this{title}) if($result > 0);
