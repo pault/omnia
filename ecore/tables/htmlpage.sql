8d2a5256209a9288adf9c3ee6f9abcc5743b014d
dbrown <dbrown@b9a9f069-900d-0410-9e71-c61feadb8a10>
2001-01-17 01:57:54 +0000

    new MIME type stuff
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@249 b9a9f069-900d-0410-9e71-c61feadb8a10

--- ecore/tables/htmlpage.sql
+++ ecore/tables/htmlpage.sql
@@ -15,5 +15,6 @@ CREATE TABLE htmlpage (
   parent_container int(11),
   ownedby_theme int(11) DEFAULT '0' NOT NULL,
   permissionneeded char(1) DEFAULT 'r' NOT NULL,
+  MIMEtype varchar(255) DEFAULT 'text/html' NOT NULL,
   PRIMARY KEY (htmlpage_id)
 );
