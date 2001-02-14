1fceaf25efcda24fa5df03c180f133e8c8dafba1
nate <nate@b9a9f069-900d-0410-9e71-c61feadb8a10>
2001-02-14 18:36:27 +0000

    added Bundle::Everything now install it and run perl -MCPAN -e shell "install Bundle::Everything" and it will take care of depenencies (TM)
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@311 b9a9f069-900d-0410-9e71-c61feadb8a10

---
+++ ebase/Bundle/Everything.pm
@@ -0,0 +1,45 @@
+package Bundle::Everything;
+$Bundle::Everything::VERSION = '0.1.1';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Everything - all the perl modules you need to install (capital E) Everything 
+
+=head1 SYNOPSIS
+
+perl Makefile.PL
+make
+make install
+
+
+=head1 CONTENTS
+
+Bundle::CPAN		
+
+Bundle::DBI		
+
+Bundle::DBD::mysql	
+
+Date::Parse		
+
+Mail::Sender
+
+Apache::DBI
+
+Devel::Peek
+
+CGI
+
+XML::Parser	-- the most likely to cause errors, because of the expat dependancy 
+
+XML::DOM 
+
+=head1 DESCRIPTION
+
+Please make sure you have the latest CPAN.pm module when executing this code
+
+=cut
