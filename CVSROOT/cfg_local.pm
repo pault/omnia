1f7320d4a6f15b7418855cef4379846364300d01
zdog <zdog@b9a9f069-900d-0410-9e71-c61feadb8a10>
2002-08-07 03:21:46 +0000

    Adding files to CVSROOT which give extra functionalities.
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@421 b9a9f069-900d-0410-9e71-c61feadb8a10

---
+++ CVSROOT/cfg_local.pm
@@ -0,0 +1,30 @@
+####################################################################
+####################################################################
+# This file contains local configuration for the CVSROOT perl
+# scripts.  It is loaded by cfg.pm and overrides the default
+# configuration in that file.
+#
+# It is advised that you test it with
+#     'env CVSROOT=/path/to/cvsroot perl -cw cfg.pm'
+# before you commit any changes.  The check is to cfg.pm which
+# loads this file.
+####################################################################
+####################################################################
+
+%TEMPLATE_HEADERS = (
+	"Reviewed by"		=> '.*',
+	"Submitted by"		=> '.*',
+	"Obtained from"		=> '.*',
+	"Approved by"		=> '.*',
+);
+
+$MAILCMD = "/usr/sbin/sendmail";
+
+$MAILADDRS='everydevel-cvs@lists.sourceforge.net';
+
+@LOG_FILE_MAP = (
+	'generallog'	=> '.*'
+);
+
+1; # Perl requires all modules to return true.  Don't delete!!!!
+#end
