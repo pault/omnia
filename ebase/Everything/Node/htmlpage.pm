699722837ef5addb12f87b719a8102cc6bf7cb1a
chromatic <chromatic@b9a9f069-900d-0410-9e71-c61feadb8a10>
2002-12-25 04:19:05 +0000

    Minor code reformattings. Updated copyright.
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@523 b9a9f069-900d-0410-9e71-c61feadb8a10

--- ebase/Everything/Node/htmlpage.pm
+++ ebase/Everything/Node/htmlpage.pm
@@ -4,7 +4,7 @@ package Everything::Node::htmlpage;
 #   Everything::Node::htmlpage
 #       Package the implements the base htmlpage functionality
 #
-#   Copyright 2000 Everything Development Inc.
+#   Copyright 2000 - 2002 Everything Development Inc.
 #   Format: tabs = 4 spaces
 #
 #############################################################################
@@ -25,19 +25,14 @@ sub insert
 {
 	my ($this, $USER) = @_;
 
- # If there is no parent container set, we need a default
- unless($$this{parent_container})
- {
- my $GNC = $$this{DB}->getNode("general nodelet container", "container");
- $$this{parent_container} = 0;
- $$this{parent_container} = $GNC if($GNC);
- }
+	# If there is no parent container set, we need a default
+	unless($$this{parent_container})
+	{
+		my $GNC = $$this{DB}->getNode("general nodelet container", "container");
+		$$this{parent_container} = $GNC ? $GNC : 0;
+	}
 
-$this->SUPER();
-
-
-
-
+	$this->SUPER();
 }
 
 #############################################################################
