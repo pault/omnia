96cb71594a8886698403fcc0fe9ab2e80a6085ec
chromatic <chromatic@b9a9f069-900d-0410-9e71-c61feadb8a10>
2004-06-15 03:45:23 +0000

    Make work with DB scheme of new installer. Minor cleanups.
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@793 b9a9f069-900d-0410-9e71-c61feadb8a10

--- ecore/scripts/postinstall.pl
+++ ecore/scripts/postinstall.pl
@@ -1,49 +1,57 @@
+use Term::ReadKey;
 
 #
 #	set up the default typeversion table for the basic install
 #
 print "setting up the typeversion table...\n";
 
-my $nodetypes = getNodeWhere ( {}, "nodetype");
+my $nodetypes = getNodeWhere( {}, "nodetype" );
 my %exceptions = ( 'user' => 1, 'document' => 1, 'nodelet' => 1 );
 
-foreach (@$nodetypes) { 
-	next if exists $exceptions{$$_{title}};
-	$DB->sqlInsert("typeversion", { typeversion_id => $$_{node_id}, version => 1 } ) 
+for my $nodetype (@$nodetypes)
+{
+	next if exists $exceptions{ $nodetype->{title} };
+	$Everything::DB->sqlInsert( "typeversion",
+		{ typeversion_id => $nodetype->{node_id}, version => 1 } );
 }
-
-use Term::ReadKey;
 
 ReadMode('noecho');
 
-my ($password, $conf) = (1..2);
+my ( $password, $conf ) = ( 1 .. 2 );
 
-do {
+do
+{
 	print "\nPlease set the root user password (max 10 chars): ";
-	$password = ReadLine 0;
-	chomp $password;
+	chomp( $password = ReadLine 0 );
+
 	print "\nPlease confirm the root user password: ";
-	$conf = ReadLine 0;
-	chomp $conf;
-} while ($password ne $conf and print "\nEr...  try again.  They don't match!"); 	
+	chomp( $conf     = ReadLine 0 );
+} while (
+	$password ne $conf and print "\nEr...  try again.  They don't match!"
+);
+
 print "\n";
 ReadMode('normal');
 
-my $U = getNode('root','user');
-$$U{passwd} = $password;
+my $U = getNode( 'root', 'user' );
+$U->{passwd} = $password;
 $U->update(-1);
 
 print "optimizing the Guest User's nodelets...\n";
-my $GU = getNode("Guest User","user");
-my $GUv = $GU->getVars();
-my $nodeletgrp = getNodeById(getNode("system settings","setting")->getVars()->{default_nodeletgroup});
+my $GU         = getNode( "Guest User", "user" );
+my $GUv        = $GU->getVars();
+my $nodeletgrp =
+	getNodeById( getNode( "system settings", "setting" )->getVars()
+		->{default_nodeletgroup} );
+
 my @newgrp;
-foreach(@{$nodeletgrp->{group}})
+for my $nl ( @{ $nodeletgrp->{group} } )
 {
-  my $nodelet = getNode($_);
-  next unless $nodelet;
-  push @newgrp,$_ if $nodelet->hasAccess($GU,"x");
+	my $nodelet = getNode( $nl );
+	next unless $nodelet;
+	push @newgrp, $nl if $nodelet->hasAccess( $GU, "x" );
 }
-$GUv->{nodelets} = join ",",@newgrp;
+
+$GUv->{nodelets} = join ",", @newgrp;
 $GU->setVars($GUv);
 $GU->update(-1);
