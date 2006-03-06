baf9aa78476e54524058600a8534a79211d82907
chromatic <chromatic@b9a9f069-900d-0410-9e71-c61feadb8a10>
2006-03-06 23:39:21 +0000

    Added Build.PL and Everything::Build helper. Added add_text_file.sh script to set Subversion properties on new files.
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@824 b9a9f069-900d-0410-9e71-c61feadb8a10

---
+++ ebase/lib/Everything/Build.pm
@@ -0,0 +1,55 @@
+package Everything::Build;
+
+use File::Find;
+use File::Path;
+use File::NCopy;
+
+use Module::Build;
+
+use vars '@ISA';
+@ISA = 'Module::Build';
+
+sub ACTION_test
+{
+	my $self = shift;
+	my @files;
+
+	find( sub { /\.t\z/ and push @files, $File::Find::name }, 't' );
+	$self->SUPER::ACTION_test( 'test_files=' . join(' ', @files ) );
+}
+
+sub ACTION_install
+{
+	my $self       = shift;
+	my $installDir = $self->{args}{installDir};
+
+	# Create the installation directory
+	eval { mkpath( $installDir ) };
+	die "Couldn't create '$installDir': $@\n" if $@;
+
+	my $file = File::Copy->new( recursive => 1 );
+
+	for my $dir (qw( nodeballs web tables bin docs images
+		everything.apache.conf ))
+	{
+		$file->copy( $dir, $installDir )
+			or warn "No files copied from $dir to $installDir";
+	}
+
+	if (my $httpconf = $self->{args}{httpconf})
+	{
+		local *CONF;
+		if ( open( CONF, '>> ' . $httpconf ))
+		{
+			print CONF $self->{args}{includestr};
+		}
+		else
+		{
+			warn "Could not append include line to '$httpconf': $!\n";
+		}
+	}
+
+	$self->SUPER::ACTION_install(@_);
+}
+
+1;
