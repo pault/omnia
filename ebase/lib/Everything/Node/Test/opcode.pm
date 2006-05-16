27fb2a3456089eeba0a1c14dc18f26eba0829379
chromatic <chromatic@b9a9f069-900d-0410-9e71-c61feadb8a10>
2006-05-16 20:20:48 +0000

    r16735@windwheel:  chromatic | 2006-05-16 13:20:39 -0700  Ported opcode tests to the new format.
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@873 b9a9f069-900d-0410-9e71-c61feadb8a10

---
+++ ebase/lib/Everything/Node/Test/opcode.pm
@@ -0,0 +1,18 @@
+package Everything::Node::Test::opcode;
+
+use strict;
+use warnings;
+
+use base 'Everything::Node::Test::htmlcode';
+use Test::More;
+
+sub test_extends :Test( +1 )
+{
+	my $self  = shift;
+	my $class = $self->node_class();
+	ok( $class->isa( 'Everything::Node::htmlcode' ),
+		'opcode should extend htmlcode' );
+	$self->SUPER();
+}
+
+1;
