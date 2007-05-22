ada464530c18f0f78777d5bc67b9c4b45a5a5a7b
paul_the_nomad <paul_the_nomad@b9a9f069-900d-0410-9e71-c61feadb8a10>
2007-05-22 23:12:39 +0000

    Nodebase connection utility function for command line and tests
    git-svn-id: https://everydevel.svn.sourceforge.net/svnroot/everydevel/trunk@958 b9a9f069-900d-0410-9e71-c61feadb8a10

--- ebase/lib/Everything/CmdLine.pm
+++ ebase/lib/Everything/CmdLine.pm
@@ -7,16 +7,18 @@ use base 'Exporter';
 use strict;
 use warnings;
 
-our @EXPORT_OK = qw(get_options abs_path);
+our @EXPORT_OK = qw(get_options abs_path usage_options make_nodebase);
 
 Getopt::Long::Configure(qw/bundling/);
 
 sub get_options {
-    my ($usage_msg) = @_;
+    my ($usage_msg, $other_options) = @_;
+    $other_options ||= [];
     my %opts;
+    $opts{database} = $opts{port} = $opts{host} = $opts{password} = $opts{user} = '';
     GetOptions(
         \%opts,         'user|u=s', 'password|p=s', 'host|h=s',
-        'database|d=s', 'port|P=s', 'type|t=s'
+        'database|d=s', 'port|P=s', 'type|t=s', @$other_options
     ) or usage_options($usage_msg);
     return \%opts;
 
@@ -27,7 +29,7 @@ sub usage_options {
     $usage_msg ||= "Usage:\n\n";
 
     $usage_msg .= <<USAGE;
-Takes the following options:
+Takes the following standard options:
 \t -d
 \t --database
 \t\t the db name. In the case of sqlite, it will be the file name of the test db, it will not be deleted on completion.  If no name is specified a temporary file will be used if possible. The temporary file will be deleted on completion.  In the case of mysql or postgresql, it is the name of the database to use.
@@ -73,6 +75,32 @@ sub abs_path {
 
     return Cwd::abs_path($file);
 
+}
+
+=cut
+
+=head2 C<make_nodebase>
+
+Takes a hash reference like the one returned by get_options(). Returns a nodebase object if it can get one.
+
+=cut
+
+sub make_nodebase {
+    my ($opts) = @_;
+
+    $$opts{type} ||= 'sqlite';
+    $$opts{user} ||= $ENV{USER};
+    $$opts{host} ||= 'localhost';
+
+    my $nb =
+      Everything::NodeBase->new(
+        "$$opts{database}:$$opts{user}:$$opts{password}:$$opts{host}",
+        1, $$opts{type} );
+    croak
+"Can't connect to nodebase using database '$$opts{database}', user '$$opts{user}', password '$$opts{password}', host '$$opts{host}' and type '$$opts{type}'"
+      unless $nb;
+
+    return $nb;
 }
 
 1;
--- ebase/lib/Everything/Test/CmdLine.pm
+++ ebase/lib/Everything/Test/CmdLine.pm
@@ -2,6 +2,8 @@ package Everything::Test::CmdLine;
 
 use Test::More;
 use Test::Warn;
+use Test::MockObject;
+use Test::Exception;
 use Cwd;
 use warnings;
 use strict;
@@ -86,6 +88,52 @@ sub test_abs_path : Test(4) {
     $rv = $test_code->('../here');
     is( $rv, $wd . '/here', '..resolves the directory "..".' );
 
+}
+
+
+sub test_make_nodebase : Test(5) {
+    my $self = shift;
+    can_ok( $self->{class}, 'make_nodebase' )
+      || return 'abs_path not implemented.';
+
+    my $mock = Test::MockObject->new;
+    $mock->fake_module('Everything::NodeBase');
+
+    my @new_args;
+    my $new_returns = $mock;
+    local *Everything::NodeBase::new;
+    *Everything::NodeBase::new = sub { @new_args = @_; return $new_returns };
+
+    my $test_code = \&{ $self->{class} . '::make_nodebase' };
+    my $opts      = {
+        database => 'dbname',
+        user     => 'dbuser',
+        password => 'dbpassword',
+        host     => 'dbhost',
+        type     => 'dbtype',
+        port     => 'dbport'
+    };
+    my $rv = $test_code->($opts);
+    is_deeply(
+        \@new_args,
+        [
+            'Everything::NodeBase', "dbname:dbuser:dbpassword:dbhost",
+            1,                      'dbtype'
+        ],
+        '...args are handled properly.'
+    );
+    is( "$rv", $mock, '...returns the return value of NodeBase\'s new.' );
+
+    $opts = { database => 'dbname', password => '' };
+    $rv = $test_code->($opts);
+    is_deeply(
+        \@new_args,
+        [ 'Everything::NodeBase', "dbname:$ENV{USER}::localhost", 1, 'sqlite' ],
+        '...defaults are set.'
+    );
+
+    undef $new_returns;
+    dies_ok { $test_code->($opts) } '...dies if no nodebase found.';
 }
 
 1;
